8d91f88b1b6711ded56f97f12680bee86dae7028
Rakeshgr962 <rakeshgr962@gmail.com>
2024-11-22 22:03:00 +0530

    Created using Colab

---
+++ Untitled0.c
@@ -0,0 +1,50 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNz6g7NWG5zmuKKmR19IZul",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Rakeshgr962/Os/blob/main/Untitled0.c\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "54sdt8xetATI"
+      },
+      "outputs": [],
+      "source": [
+        "#include <stdio.h>\n",
+        "//Arear of square\n",
+        "int main ()\n",
+        "{\n",
+        "    int side;\n",
+        "    printf(\"Enter side:\");\n",
+        "    scanf(\"%d\", &side);\n",
+        "    printf(\"Area of square is: %d\", side*side);\n",
+        "    return 0;\n",
+        "}"
+      ]
+    }
+  ]
+}
